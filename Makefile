942602948be7dd73aa26f8955f6544aae965d17d
Wei Dong <wdong@aaalgo.com>
2021-02-13 13:25:28 -0500

    update

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-CXXFLAGS += -std=c++17 -O3 -static \
+CXXFLAGS += -std=c++17 -O3 -g -static \
 	    -I3rd/glob/single_include \
 	    -I3rd/fmt/include \
 	    -I3rd/spdlog/include \
--- ehash.cpp
+++ ehash.cpp
@@ -13,12 +13,10 @@
 #define SPDLOG_FMT_EXTERNAL
 #include <spdlog/spdlog.h>
 
-namespace fs=std::filesystem;
-
 using std::string;
-using std::cout;
-using std::endl;
 using std::vector;
+
+namespace fs=std::filesystem;
 
 class sink: std::ofstream {
     std::mutex mutex;
@@ -31,7 +29,9 @@ public:
     }
 };
 
-string extract_key (char *buf, char *buf_end, int key, char delim) {
+std::hash<std::string> HASH;
+
+size_t hash_key (char *buf, char *buf_end, int key, char delim) {
     // skip key - 1 items
     for (int i = 0; i < key; ++i) {
         while ((buf < buf_end) && (buf[0] != delim)) ++buf;
@@ -40,7 +40,7 @@ string extract_key (char *buf, char *buf_end, int key, char delim) {
     }
     char *end = buf;
     while ((end < buf_end) && (end[0] != delim)) ++end;
-    return string(buf, end);
+    return HASH(string(buf, end));
 }
 
 void ensure_dir (fs::path const &dir) {
@@ -58,7 +58,6 @@ int main (int argc, char *argv[]) {
     int number = 100;
     char line_delim = '\n';
     char field_delim = '\t';
-    size_t buf_size = 32 * 1024 * 1024;
     int key = 0;
     string batch;
     string loader;
@@ -71,13 +70,12 @@ int main (int argc, char *argv[]) {
 
     {
         CLI::App cli{"ehash"};
-        cli.add_option("-n,--number", number, "");
-        cli.add_option("-d,--delimiter", field_delim, "field delimiter, default '\t'.");
-        cli.add_option("--format", output_format, "output filename format");
-        cli.add_option("-o,--output", output_dir, "output directory");
-        cli.add_option("-b,--batch", batch, "batch");
-        cli.add_option("-l,--loader", loader, "loader");
-        cli.add_option("--buffer", buf_size, "buffer size");
+        cli.add_option("-n,--number", number, "number of partitions (100)");
+        cli.add_option("-d,--delimiter", field_delim, "field delimiter ('\t').");
+        cli.add_option("--format", output_format, "output filename format (part-{:0>5d})");
+        cli.add_option("-o,--output", output_dir, "output directory (ehash_out)");
+        cli.add_option("-b,--batch", batch, "batch (default empty)");
+        cli.add_option("-l,--loader", loader, "loader (default empty)");
         cli.add_option("input", inputs, "input directories");
         cli.add_flag_function("-v", [&log_level](int v){ log_level -= v;}, "verbose");
         cli.add_flag_function("-V", [&log_level](int v){ log_level += v;}, "less verbose");
@@ -97,33 +95,9 @@ int main (int argc, char *argv[]) {
         }
     }
 
-    vector<string> jobs;
-    for (auto const &input: inputs) {
-        jobs.push_back(input);
-        /*
-        spdlog::debug("scanning {}", input_dir.string());
-        if (fs::is_regular_file(input_dir)) {
-            spdlog::debug("adding {}", input_dir.string());
-            jobs.push_back(input_dir);
-            continue;
-        }
-#if 0
-        for (auto const &path: glob::rglob(input_dir.string() + "/**")) {
-            spdlog::debug("testing {}", path.string());
-            if (fs::is_regular_file(path)) {
-                spdlog::debug("adding {}", path.string());
-                jobs.push_back(path);
-            }
-        }
-#else
-        spdlog::error("{} is not a regular file.", input_dir.string());
+    spdlog::info("{} inputs found.", inputs.size());
 
-#endif
-*/
-    }
-    spdlog::info("{} input inputs found.", jobs.size());
-
-    if (jobs.empty() || dry) return 0;
+    if (inputs.empty() || dry) return 0;
 
     ensure_dir(output_dir);
     vector<std::unique_ptr<sink>> sinks;
@@ -142,17 +116,18 @@ int main (int argc, char *argv[]) {
         sinks.emplace_back(std::make_unique<sink>(output_path));
     }
 
-    std::hash<std::string> hash_fn;
 #pragma omp parallel schedule(dynamic, 1)
-    for (int i = 0; i < jobs.size(); ++i) {
-        auto const &path = jobs[i];
-        spdlog::debug("processing {}", path);
+    for (int i = 0; i < inputs.size(); ++i) {
+        auto const &input = inputs[i];
         FILE *stream;
         if (loader.empty()) {
-            stream = fopen(path.c_str(), "rb");
+            spdlog::debug("loading {}", input);
+            stream = fopen(input.c_str(), "r");
         }
         else {
-            stream = popen("", "rb");
+            string cmd = fmt::format("{} {}", loader, input);
+            spdlog::info("streaming {}", cmd);
+            stream = popen(cmd.c_str(), "r");
         }
         if (!stream) throw 0;
         char *lineptr = NULL;
@@ -160,8 +135,7 @@ int main (int argc, char *argv[]) {
         for (;;) {
             ssize_t r = getdelim(&lineptr, &len, line_delim, stream);
             if (r <= 0) break;
-            string v = extract_key(lineptr, lineptr+r, key, field_delim);
-            int part = hash_fn(v);
+            int part = hash_key(lineptr, lineptr+r, key, field_delim) % sinks.size();
             sinks[part]->add(lineptr, r);
         }
         free(lineptr);
